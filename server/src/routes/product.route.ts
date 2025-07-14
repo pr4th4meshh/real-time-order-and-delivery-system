edf3a50353a6bc7f56320d952f2ad9c438427cdc
pr4th4meshh <prathameshasolkar17@gmail.com>
2025-07-14 11:48:26 +0530

    chore: remove requireAuth for GETting products

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -4,8 +4,8 @@ import { requireAuth, requireRole } from "../middleware";
 
 const router = Router();
 
-router.get("/all", requireAuth, getProducts);
-router.get("/:id", requireAuth, getProductById);
+router.get("/all", getProducts);
+router.get("/:id", getProductById);
 router.post("/create", requireAuth, requireRole(["admin"]), createProduct);
 router.patch("/:id", requireAuth, requireRole(["admin"]), updateProduct);
 router.delete("/:id", requireAuth, requireRole(["admin"]), deleteProduct);
